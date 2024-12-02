52bf7108f984f45dfb27e6e4291c531b3ede6e4f
Peter Lithammer <peter.lithammer@gmail.com>
2024-12-02 22:20:45 +0100

    Add script to get problem input

---
+++ get-input.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+[ -z "$1" ] && echo "Usage: ${0##*/} <day>" && exit 1
+
+DAY=$(printf '%d' "$1")
+PADDED_DAY=$(printf '%02d' "$DAY")
+DEST="day$PADDED_DAY.txt"
+
+curl -s --cookie "session=$(cat session)" "https://adventofcode.com/2024/day/$DAY/input" >"$DEST"
+
+head -3 "$DEST"
+echo '[...]'
+tail -3 "$DEST"
